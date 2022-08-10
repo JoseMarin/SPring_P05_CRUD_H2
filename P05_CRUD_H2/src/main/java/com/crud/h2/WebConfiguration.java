c29ce123cf96d54f0bc24ec6656244a560ae93be
Jose Marín <jmarinmalagon@gmail.com>
2022-08-10 12:29:37 +0200

    WebConfig Cors

---
+++ P05_CRUD_H2/src/main/java/com/crud/h2/WebConfiguration.java
@@ -0,0 +1,15 @@
+package com.crud.h2;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("*").allowedOrigins("*").allowedMethods("*");
+    }
+}
+
